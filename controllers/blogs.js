32330fe3a3756fdfdae32112a445585dd560ec78
Miika Valkonen <miika.valkonen08@gmail.com>
2026-09-17 13:22:39 +0300

    6. Moar likes

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 const { Blog } = require('../models')
 
 const blogFinder = async (req, res, next) => {
-    req.blog = await Blog.findByPk(req.paramsq.id)
+    req.blog = await Blog.findByPk(req.params.id)
     if (!req.blog) {
         return res.status(404).end()
     }
@@ -27,6 +27,12 @@ router.post('/', async (req, res) => {
 router.delete('/:id', blogFinder, async (req, res) => {
   await req.blog.destroy()
   res.status(204).end()
+})
+
+router.put('/:id', blogFinder, async (req, res) => {
+    req.blog.likes = req.body.likes
+    await req.blog.save()
+    res.json(req.blog)
 })
 
 module.exports = router
