d24076a294597c163c43e8c1abfa550838d6e220
Miika Valkonen <miika.valkonen08@gmail.com>
2026-09-17 11:34:16 +0300

    4. Web service is born

--- cli.js
+++ cli.js
@@ -1,5 +1,8 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
+const express = require('express')
+const app = express()
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
@@ -9,16 +12,62 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 });
 
-const main = async () => {
-  try {
-    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-    blogs.forEach(blog => {
-        console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
-    })  
-    sequelize.close()  
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
   }
-}
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+Blog.sync()
 
-main()
+app.use(express.json())
+
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const blog = await Blog.create({...req.body})
+    return res.json(blog)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+  if (!blog) {
+    return res.status(400).end()
+  }
+  await blog.destroy()
+  return res.status(204).end()
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
