ab8c5af99166ddfbc24c9eb44eb55cd204c0c86a
Miika Valkonen <miika.valkonen08@gmail.com>
2026-09-23 12:22:38 +0300

    Enhance blog retrieval to support search functionality by title

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken')
 
 const { SECRET } = require('../util/config')
 const { Blog, User } = require('../models')
+const { Op } = require('sequelize')
 
 const tokenExtractor = (req, res, next) => {
   const authorization = req.get('authorization')
@@ -32,12 +33,21 @@ const blogFinder = async (req, res, next) => {
 }
 
 router.get('/', async (req, res) => {
+  const where = {}
+
+  if (req.query.search) {
+    where.title = {
+      [Op.iLike]: `%${req.query.search}%`
+    }
+  }
+
   const blogs = await Blog.findAll({
     attributes: { exclude: ['userId'] },
     include: {
       model: User,
       attributes: ['name']
-    }
+    },
+    where
   })
   res.json(blogs)
 })
